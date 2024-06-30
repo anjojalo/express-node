1c16eab7281f0d7d96dcf83f5de35fa8e33f8e00
anjojalo <anjojalo@hotmail.com>
2024-06-30 15:34:04 -0500

    first commit

---
+++ src/scripts/myscript.js
@@ -0,0 +1,13 @@
+import Prism from 'prismjs';
+
+
+
+
+import 'prism-themes/themes/prism-material-dark.css';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/plugins/custom-class/prism-custom-class';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
+Prism.highlightAll();
